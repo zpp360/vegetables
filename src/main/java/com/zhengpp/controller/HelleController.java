ad7517ac84b9ac72602b31c3ea46d3b81924439b
zpp360 <184458308@qq.com>
2017-10-10 20:37:30 +0800

    init

--- src/main/java/com/zhengpp/controller/HelleController.java
+++ src/main/java/com/zhengpp/controller/HelleController.java
@@ -1,6 +1,7 @@
 package com.zhengpp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,9 @@ public class HelleController {
     @RequestMapping("hello")
     public String hello(){
         return content;
+    }
+    @GetMapping("index")
+    public String index(){
+        return "";
     }
 }
